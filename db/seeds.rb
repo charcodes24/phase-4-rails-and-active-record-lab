6249b64e4a219717984561c93dc8044c82aa2365
charcodes24 <charcodes24@gmail.com>
2021-08-16 10:20:16 -0700

    completed assignment.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name: "Charlene", last_name: "Johnson", grade: 10)
+Student.create!(first_name: "Teresa", last_name: "Yeo", grade: 10)
+Student.create!(first_name: "Shannon", last_name: "Carbajal", grade: 10)
+Student.create!(first_name: "Alex", last_name: "Baroso", grade: 10)
